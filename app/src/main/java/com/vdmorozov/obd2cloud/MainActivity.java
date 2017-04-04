99dbd06bd35289ffa15abd28e33f7fe0c3bf1c11
vdmorozov <v.d.morozov@yandex.ru>
2017-04-04 22:32:10 +0300

    главный экран, начало

--- app/src/main/java/com/vdmorozov/obd2cloud/MainActivity.java
+++ app/src/main/java/com/vdmorozov/obd2cloud/MainActivity.java
@@ -1,8 +1,14 @@
 package com.vdmorozov.obd2cloud;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +18,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    private class DownloadImageTask extends AsyncTask<Uri, Void, Bitmap> {
+        ImageView bmImage;
+
+        DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(Uri... photoUrls) {
+            Uri photoUrl = photoUrls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(
+                        "https://lh3.googleusercontent.com/-198VvqhnzFE/AAAAAAAAAAI/AAAAAAAAAAA/AMcAYi8nFm2kujWLjoDR-WRXZC-59CCX7Q/s96-c/photo.jpg"
+                ).openConnection().getInputStream();
+                if(in != null){
+                    mIcon11 = BitmapFactory.decodeStream(in);
+                }
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            if(result != null) {
+                Log.w("LOL", result.toString());
+                bmImage.setImageBitmap(result);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +73,31 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        }
 
+        Uri profileImageUrl = mFirebaseUser.getPhotoUrl();
+
+        //todo: приходит null - найти способ дёрнуть нужную ImageView
+        ImageView profileImageView = (ImageView) findViewById(R.id.profileImage);
+
+        if (profileImageUrl != null) {
+            new DownloadImageTask(profileImageView)
+                    .execute(profileImageUrl);
+        }
+
+        //добавление кнопки для вызова бокового меню в хэдере
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -44,6 +106,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
+        //закрытие бокового меню по нажатию кнопки "назад"
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
@@ -66,10 +129,10 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
